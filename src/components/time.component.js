440fb8e613f4ef543df910744e9aa30f1f37775e
Mike Walshe <mjwalshe@gmail.com>
2020-12-24 16:33:14 +0000

    Add basic second incrementer

--- src/components/time.component.js
+++ src/components/time.component.js
@@ -5,8 +5,34 @@ export default class Time extends Component {
     super(props);
     this.state = {
       title: this.props.title,
-      seconds: this.props.seconds
+      seconds: this.props.seconds,
+      startTime: null
     }
+    this.updateTime=this.updateTime.bind(this);
+  }
+
+
+
+  componentDidMount() {
+    this.startTime()
+  }
+
+  startTime() {
+    this.setState({
+      startTime: new Date()
+    })
+    setInterval(this.updateTime, 1000);
+  }
+
+  updateTime() {
+    let date = new Date()
+    console.log(date.getSeconds());
+    // console.log("state: " + this.state.title);
+    console.log(this.state.startTime.getSeconds());
+    let updatedSeconds = date.getSeconds() - this.state.startTime.getSeconds();
+    this.setState({
+      seconds: updatedSeconds
+    })
   }
 
   render() {
