454ddf37454033ca39ab75e527b31f3a32ae4506
mikjw <mjwalshe@gmail.com>
2021-01-16 14:58:06 +0000

    Add form to login

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      email: "",
+      password: ""
+    }
+
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  onChangeEmail(e) {
+    this.setState({email: e.target.value});
+  }
+
+  onChangePassword(e) {
+    this.setState({password: e.target.value});
+  }
+
+  submitForm(e) {
+      axios.post('http://localhost:5001/login', {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(res => console.log(res.data));
   }
 
   render() {
@@ -12,6 +36,11 @@ export default class Login extends Component {
         <h1>
           Login
         </h1>
+          <label> Email: </label> 
+            <input type="text" name="email" value={this.state.email} onChange={this.onChangeEmail} />
+          <label> Password: </label> 
+            <input type="text" name="password" value={this.state.password} onChange={this.onChangePassword} /> 
+          <button onClick={() => {this.submitForm()}}>Submit</button>
       </div>
     );
   }
