df1317a78a03e499e46f85b9b6a62d00a0ef4151
mikjw <mjwalshe@gmail.com>
2021-02-21 22:11:16 +0000

    Add request user log button

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -24,15 +24,29 @@ export default class Login extends Component {
 
   submitForm(e) {
       axios.post('http://localhost:5001/login', {
+        withCredentials: true,
         email: this.state.email,
         password: this.state.password
       })
-      .then(res => console.log(res.data));
+      .then(res => {
+        console.log(res.data);
+      });
   }
+
+getUser() {
+  axios.get('http://localhost:5001/requser', {
+    withCredentials: true
+  })
+  .then((res) => {
+      this.setState({requser: res.data});
+      console.log('this.state.requser: ', this.state.requser);
+    });
+  };
 
   render() {
     return (
       <div>
+        <div>
         <h1>
           Login
         </h1>
@@ -41,6 +55,13 @@ export default class Login extends Component {
           <label> Password: </label> 
             <input type="password" name="password" value={this.state.password} onChange={this.onChangePassword} /> 
           <button onClick={() => {this.submitForm()}}>Submit</button>
+        </div>
+        <div>
+        <h1>
+          Get user
+        </h1>
+          <button onClick={() => {this.getUser()}}>Submit</button>
+        </div>
       </div>
     );
   }
