ba416eef5cf395b19c3ada51edb573edc1fec137
mikjw <mjwalshe@gmail.com>
2021-03-22 00:56:39 +0000

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
 import { BrowserRouter as Router, Route} from "react-router-dom";
-
-import TimeList from './components/timeList.component'
-import Register from "./components/register.component"
-import Login from "./components/login.component";
+import Register from './components/register.component';
+import Login from './components/login.component';
+import HomeScreen from './components/homeScreen.component';
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <div className="container">
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/times" component={TimeList} />
+      <div className="Header">
+        <img id="logo" src="/timey.png" alt="timey-logo" />
       </div>
-    </Router>
+      <div className='Router'>
+        <Router>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/times" component={HomeScreen} />
+        </Router>
+      </div>
     </div>
   );
 }
